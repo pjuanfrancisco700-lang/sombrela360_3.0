3686749a4358e163319aa5f6c55f98dd421a2b40
pjuanfrancisco700-lang <pjuanfrancisco700@gmail.com>
2026-08-22 23:37:17 -0600

    Update config.js

--- config.js
+++ config.js
@@ -12,6 +12,6 @@
 window.SOMBRELA_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbxAuCTqlMqV1y_1zM4YpFwhtyRHb5mNzm78HmaeMuijye9jbMitnAIlQqbAma6K4w/exec",
   APP_NAME: "Sombrela 360",
-  DEMO_MODE: true,
+  DEMO_MODE: false,
   REQUEST_TIMEOUT_MS: 18000
 };
