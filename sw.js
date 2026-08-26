6071ff5c5a0d9db4a3773e380903d297c58ab749
pjuanfrancisco700-lang <pjuanfrancisco700@gmail.com>
2026-08-25 22:11:26 -0600

    Update cache version and app.js query parameter

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = "sombrela360-v3.0.18";
+const CACHE = "sombrela360-v3.0.19";
 
 const STATIC = [
   "./",
   "./index.html",
   "./styles.css?v=3.0.14",
   "./config.js",
-  "./app.js?v=3.0.18",
+  "./app.js?v=3.0.19",
   "./manifest.json",
   "./assets/icon-192.png",
   "./assets/icon-512.png",
